84ae88d9fc27268c9c6abba4e9f54831d72e8634
Sinan Balıbey <sinanbey23@hotmail.com>
2023-08-10 11:16:52 +0300

    JavaLoops

--- Java/JavaLoops1/src/Main.java
+++ Java/JavaLoops1/src/Main.java
@@ -1,5 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for(int i=1;i<=10;i++){
+            System.out.println(N+" x "+i+" = "+i*N);
+        }
+
+        bufferedReader.close();
     }
 }
