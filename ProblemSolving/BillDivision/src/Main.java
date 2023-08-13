f39cc5a5e66dc57c9fcaba1cc6fe02b6067a97f0
Sinan Balıbey <sinanbey23@hotmail.com>
2023-08-13 10:21:44 +0300

    BillDivision

--- ProblemSolving/BillDivision/src/Main.java
+++ ProblemSolving/BillDivision/src/Main.java
@@ -18,7 +18,21 @@ class Result {
 
     public static void bonAppetit(List<Integer> bill, int k, int b) {
         // Write your code here
+        //k=annanın yemediği
+        //b annanın ödüyeceği
 
+        int n=bill.size();
+        int toplamAnna=0;
+        for(int i=0;i<n;i++){
+            toplamAnna+=bill.get(i);
+        }
+
+        if((toplamAnna-bill.get(k))/2==b){
+            System.out.println("Bon Appetit");
+        }
+        else {
+            System.out.println(b-(toplamAnna-bill.get(k))/2);
+        }
     }
 
 }
