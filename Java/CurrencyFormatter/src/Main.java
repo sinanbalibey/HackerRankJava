90a91c300d885daba1da475805893c44c3008577
Sinan Balıbey <sinanbey23@hotmail.com>
2023-08-20 23:15:54 +0300

    CurrencyFormatter

---
+++ Java/CurrencyFormatter/src/Main.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+            Scanner scan=new Scanner(System.in);
+            double payment=scan.nextDouble();
+            scan.close();
+            System.out.println(payment);
+
+
+
+
+
+    }
+}
