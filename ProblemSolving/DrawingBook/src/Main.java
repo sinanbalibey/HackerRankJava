a8d36ffeb7a70513bb19df7e4adb08a472e68916
Sinan Balıbey <sinanbey23@hotmail.com>
2023-08-26 11:40:48 +0300

    DrawingBook

--- ProblemSolving/DrawingBook/src/Main.java
+++ ProblemSolving/DrawingBook/src/Main.java
@@ -1,22 +1,93 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
-       int a=Result.pageCount(5,4);
-        System.out.println(a);
+        //1,2-3,4-5,6-7;
+        //1,2-3,4-5,6-7,8;
+
+        int n=4;
+        int p=4;
+        int count1=0;
+        int count2=0;
+        if(n%2==0){
+            for(int i=1;i<n;i=i+2){
+                if(p==i||p==i-1)
+                    break;
+
+                count1++;
+            }
+            for(int i=n;i>=1;i=i-2){
+                if(p==i||p==i+1)
+                    break;
+
+                count2++;
+            }
+        }else {
+            for(int i=1;i<n;i=i+2){
+                if(p==i||p==i-1)
+                    break;
+
+                count1++;
+            }
+            for(int i=n;i>=1;i=i-2){
+                if(p==i||p==i-1)
+                    break;
+
+                count2++;
+            }
+        }
+
+
+
+        System.out.println("count1: "+count1);
+        System.out.println("count2: "+count2);
+
     }
 }
 class Result {
     public static int pageCount(int n, int p) {
-        // Write your code here
         //n=kitabın uzunluğu
         //p geçmek istediği sayfa sayısı
         //return çevirilecek sayfa sayısı
-        int count=0;
-        for(int i=2;i<n;i=i+2){
-            count++;
-            if(i==p||i+1==p)
-                break;
+        int count1=0;
+        int count2=0;
+        if(n%2==0){
+            for(int i=1;i<n;i=i+2){
+
+                if(p==i||p==i-1)
+                    break;
+                count1++;
+            }
+            for(int i=n;i>=1;i=i-2){
+
+                if(p==i||p==i+1)
+                    break;
+
+                count2++;
+            }
+        }else{
+            for(int i=1;i<n;i=i+2){
+                if(p==i||p==i-1)
+                    break;
+
+                count1++;
+            }
+            for(int i=n;i>=1;i=i-2){
+                if(p==i||p==i-1)
+                    break;
+
+                count2++;
+            }
         }
-        return count;
+
+        if(n%2==1&&p==n-1){
+            count2=0;
+        }
+
+        return count1<=count2?count1:count2;
     }
 
 }
