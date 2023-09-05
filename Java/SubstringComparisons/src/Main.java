466e3fc8e41965ba87eba29fdab23bc11315b01b
Sinan Balıbey <sinanbey23@hotmail.com>
2023-09-06 00:24:00 +0300

    SubstringComparisons

---
+++ Java/SubstringComparisons/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+
+        return smallest + "\n" + largest;
+    }
+}
