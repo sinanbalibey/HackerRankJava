28d2d876dc4fac9c1fc589814e75e2c63fc3f0b3
Sinan Balıbey <sinanbey23@hotmail.com>
2023-09-20 08:14:19 +0300

    ClimbingTheLeaderboard

---
+++ ProblemSolving/ClimbingTheLeaderboard/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        //6 4 2 1
+        List<Integer> ranked=new ArrayList<>();
+        ranked.add(100);
+        ranked.add(100);
+        ranked.add(50);
+        ranked.add(40);
+        ranked.add(40);
+        ranked.add(20);
+        ranked.add(10);
+
+        List<Integer> player=new ArrayList<>();
+        player.add(5);
+        player.add(25);
+        player.add(50);
+        player.add(120);
+
+
+        for(int i=0;i<ranked.size();i++){
+            if(player.get())
+
+        }
+        System.out.println("ranked: "+ ranked);
+
+    }
+
+
+    public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
+        // Write your code here
+
+        return null;
+    }
+
+}
